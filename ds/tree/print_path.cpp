e66b4febcbe2e475e668c65313bb41677b1e7704
Pankaj Trivedi <trivedipankaj8@gmail.com>
2011-07-08 00:09:27 +0530

    the one with printing all paths from root in a Binary Tree

---
+++ ds/tree/print_path.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+
+struct node{
+       int data;
+       node *left;
+       node *right;
+};
+
+node *newNode(int data){
+     node *nn = new node;
+     nn->data  = data;
+     nn->left = NULL;
+     nn->right = NULL;
+     return nn;
+}
+
+node *insert(node *root, int data){
+     if(root ==NULL)
+        return newNode(data);
+     else if( root->data >= data)
+         root->left  = insert(root->left,data);           
+     else root->right = insert(root->right,data);
+     return root;
+}
+
+void inorder ( node *root){
+     if(root != NULL){
+             inorder(root->left);
+             cout<<root->data<<" ";
+             inorder(root->right);
+     }     
+}
+
+void preorder ( node *root){
+     if(root != NULL){
+             cout<<root->data<<" ";
+             preorder(root->left);
+             preorder(root->right);
+     }     
+}
+
+void mirror_tree(node *root){
+          if(root !=NULL){
+                  mirror_tree(root->left); 
+                  mirror_tree(root->right);
+                  
+                  node *temp = root->left;
+                  root->left = root->right;
+                  root->right  = temp; 
+        
+          }             
+          
+}
+
+
+void print_path(node *root, int path[], int len){
+           if(root ==NULL) return;
+           path[len++] = root->data;
+           
+           if(root->left ==NULL && root->right ==NULL){
+                  for(int i=0; i<len; i++)
+                       cout<<path[i]<<" ";
+                  cout<<endl;
+           }
+           print_path(root->left,path,len);
+           print_path(root->right,path,len);
+          
+}
+
+main(){
+       node *d = NULL;
+       node *p =NULL;
+       int a[] = {30,33,14,3,5,78,9,32,1,54};
+       int b[] = {30,33,14,3,5,78,32,1,54};
+       int   n1  = sizeof(a)/sizeof(a[0]);
+       int n2 = sizeof(b)/sizeof(b[0]);
+       for(int i=0; i<n1; i++){
+               d = insert(d,a[i]);               
+       }
+       for(int i=0; i<n2; i++){
+               p = insert(p,b[i]);               
+       }
+       int path[1000];
+       print_path(d,path,0);
+      // inorder(d); cout<<endl;
+       // mirror_tree(d);
+      // inorder(d); cout<<endl;
+             system("pause");
+}
+
